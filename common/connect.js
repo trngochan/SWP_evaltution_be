7e324f0e41dcfca419f5116afbadb9b7caab7205
trngochan <85632212+trngochan@users.noreply.github.com>
2023-07-17 13:32:51 +0700

    han push

--- common/connect.js
+++ common/connect.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "test",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect(function (err) {
